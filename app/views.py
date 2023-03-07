a89a7c444cfa5fd251252d3c2d3038498ac23349
Devinn <devinnweller@yahoo.com>
2023-03-06 19:34:34 -0500

    Made some changes

--- app/views.py
+++ app/views.py
@@ -7,7 +7,8 @@ This file contains the routes for your application.
 
 from app import app
 from flask import render_template, request, redirect, url_for
-
+from app.models import PropertyProfile
+from app.forms import PropertyForm
 
 ###
 # Routing for your application.
