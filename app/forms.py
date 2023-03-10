9e4fa1d14b2d8c9f7c0c34f23deb59aeb2b6441d
Devinn <devinnweller@yahoo.com>
2023-03-10 10:31:20 -0500

    Made some changes

--- app/forms.py
+++ app/forms.py
@@ -6,7 +6,7 @@ from flask_wtf.file import FileField, FileRequired, FileAllowed
 class PropertyForm(FlaskForm):
     title = StringField('Property Title', validators=[InputRequired()])
     bedroom = StringField('No. of Rooms', validators=[InputRequired()])
-    bathroom = StringField('No. of Bathrooms', validators=[InputRequired()])
+    bathroom = FloatField('No. of Bathrooms', validators=[InputRequired()])
     location = StringField('Location', validators=[InputRequired()])
     price = FloatField('Price', validators=[InputRequired()])
     type = SelectField('Property Type', validators=[InputRequired()], choices=[("House","House"), ("Apartment", "Apartment")])
